24cf737f656ffa34e342bb86fdae50581fc89707
mcfreund <mfreundc@gmail.com>
2021-12-30 14:24:29 -0600

    Create schaefer atlas data
    - 7 and 17 network solutions
    - all parcellations (100 parc, ..., 1000 parc)
    - fsaverage, fsaverage5, fsaverage6

---
+++ tests/testthat/test-schaefer2018.R
@@ -0,0 +1,18 @@
+test_that("all schaefer atlas files are valid atlas objects", {
+
+  nparcs <- seq(100, 1000, 100)
+  nnetws <- c(7, 17)
+  spaces <- c("fsaverage", "fsaverage5", "fsaverage6")
+  for (nparc_i in seq_along(nparcs)) {
+    for (nnetw_i in seq_along(nnetws)) {
+      for (space_i in seq_along(spaces)) {
+        atlas <- load_atlas(paste0("schaefer2018_", nnetws[nnetw_i], "_", nparcs[nparc_i]), space = "fsaverage5")
+        expect_equal(validate_atlas(atlas), atlas)
+        rm(atlas)
+      }
+    }
+  }
+
+
+})
+
