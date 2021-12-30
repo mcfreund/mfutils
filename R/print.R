33bc8068090e09870a9e16cc28cc65c242e59135
mcfreund <mfreundc@gmail.com>
2021-12-30 15:18:28 -0600

    Add doc to print.atlas
    - automatically add to NAMESPACE

--- R/print.R
+++ R/print.R
@@ -1,3 +1,9 @@
+#' Print method for "atlas" class
+#'
+#' @param x An object of class "atlas"
+#' @param ... Other arguments passed to or from other methods
+#'
+#' @export
 print.atlas <- function(x, ...) {
   stopifnot(is_atlas(x))
   cat(x$name, "in", paste0(x$space, ":"), "\n")
